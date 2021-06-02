aea04a4a6e4fd0ec91cdfbc606a5b5d3bc5533be
DhineshGitHub <dineshmurugesan7373@gmail.com>
2021-06-02 15:45:36 +0530

    JS Demo

---
+++ JAVASCRIPT/JS/demo01.js
@@ -0,0 +1,44 @@
+var placeToVist;
+var placeToBuy;
+
+placeToBuy = null;
+
+console.log(placeToVist);
+console.log(placeToBuy);
+
+
+var a = 5, b = 5;
+
+var A = "A", B = "A";
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(b--);
+console.log(--b);
+console.log(--a);
+
+console.log(a+=a);
+
+console.log(typeof(a));
+
+console.log(a==b);
+console.log(A==B);
+
+console.log(A!=B);
+
+console.log(a > b);
+console.log(a <= b);
+console.log(A&&B);
+
+console.log(typeof(A));
+
+console.log("Hello" + 2);
+console.log("Hello" + " " + "World!");
+console.log("Trip" + " " + undefined);
+console.log("Trip" + " " + null);
+
+console.log(typeof "String");
